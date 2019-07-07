65b00f494b8f56e966b49a277cdbf5c397aa8434
jamilkhan98 <44854522+jamilkhan98@users.noreply.github.com>
2019-07-06 18:26:11 -0700

    Added comments on all methods

--- Card.cpp
+++ Card.cpp
@@ -8,21 +8,28 @@
 
 using namespace std;
 
+//Default constructor initializes rank and suit of Card object
 Card::Card()
 {
 	rank = 'Z';
 	suit = 'Z';
 }
+
+//Overloaded constructor takes in rank and suit parameters for Card object
 Card::Card(int r, char s)
 {
 	rank = r;
 	suit = s;
 }
+
+//setter method to set updated rank and suit of Card object
 void Card::setCard(int r, char s)
 {
 	rank = r;
 	suit = s;
 }
+
+//getter method to return rank of Card object
 int Card::getValue()
 {
 	if (rank <= 10) {
@@ -32,6 +39,8 @@ int Card::getValue()
 		return 10;
 	}
 }
+
+//display method for Card object, showing rank and suit
 void Card::showCard()
 {
 	if (rank == 1) {
